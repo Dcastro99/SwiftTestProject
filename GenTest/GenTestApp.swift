3134cd34f3e9699a224c9301ca6d060d1dbf87aa
Danny <93397389+Dcastro99@users.noreply.github.com>
2024-03-05 10:36:29 -0800

    Initial Commit

---
+++ GenTest/GenTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenTestApp.swift
+//  GenTest
+//
+//  Created by Danny Castro on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct GenTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
